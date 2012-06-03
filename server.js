73d8e35a902265ad97428950a1ceb786098c07f3
ide user ide_narolc <cloranswtor@gmail.com>
2012-06-03 16:08:07 +0000

    console logging

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http");
+var port = process.env.PORT;
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(port);
+
+console.log("Server has started.");
