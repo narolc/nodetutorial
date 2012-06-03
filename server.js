6171348302561a74e44119e2db166bd9827e925b
narolc <narolc>
2012-06-03 16:24:56 +0000

    refactoring onRequest

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require("http");
 var port = process.env.PORT;
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
     console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(port);
+}
 
+http.createServer(onRequest).listen(port);
 console.log("Server has started.");
