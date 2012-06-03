b0dd3348c143402ecd37f96dfcf8b490ea07b85f
narolc <narolc>
2012-06-03 16:29:04 +0000

    module-izing server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require("http");
 var port = process.env.PORT;
 
-function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(port);
+    console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(port);
-console.log("Server has started.");
+exports.start = start;
